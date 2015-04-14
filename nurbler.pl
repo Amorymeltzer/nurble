a86aa0f9a07675c849eb324d4b6a55cd4271d7dc
Amory Meltzer <Amorymeltzer@gmail.com>
2015-04-14 08:27:52 -0700

    More tidying

--- nurbler.pl
+++ nurbler.pl
@@ -10,45 +10,40 @@ use warnings;
 use diagnostics;
 
 unless (@ARGV == 2)
-{
+  {
     print "Usage: $0 <part_of_speech.txt> <file_to_nurble.txt>/n";
     exit;
-}
+  }
 
-my $output = 'nurbled_text.txt'; # new filename
-my %speech; # hash to hold part of speech lookup
+my $output = 'nurbled_text.txt';
 
+
+# Part of speech lookup
+my %speech;
 open my $pos, '<', "$ARGV[0]" or die $!;
-
-while (<$pos>)
-{
-    chomp;
-    my @tmp = split /\t/;
-    $tmp[1] =~ s/\|//go; # get rid of | character
-    $speech{$tmp[0]} = $tmp[1];
+while (<$pos>) {
+  chomp;
+  my @tmp = split /\t/;
+  $tmp[1] =~ s/\|//go;		# get rid of | character
+  $speech{$tmp[0]} = $tmp[1];
 }
-close $pos;
+close $pos or die $!;
 
 #open (my $new, ">$output") or die $!; # output nurbled text
 open my $old, '<', "$ARGV[1]" or die $!;
+while (<$old>) {
+  chomp;
+  my @tmp = split /\s/;
 
-while (<$old>)
-{
-    chomp;
-    my @tmp = split /\s/;
-
-    for my $i (0..(scalar @tmp - 1))
-    {
-#	print "\t$tmp[$i]\n";
-	if (($speech{$tmp[$i]}) && ($speech{$tmp[$i]} =~ m/N/i))
-	{
-	    print "$tmp[$i] ";
-#	    print "\n$tmp[$i]\t$speech{$tmp[$i]}\n\n";
-	}
-	else
-	{
-	    print 'nurble ';
-	}
+  for my $i (0..(scalar @tmp - 1)) {
+    #	print "\t$tmp[$i]\n";
+    if (($speech{$tmp[$i]}) && ($speech{$tmp[$i]} =~ m/N/i)) {
+      print "$tmp[$i] ";
+      #	    print "\n$tmp[$i]\t$speech{$tmp[$i]}\n\n";
+    } else {
+      print 'nurble ';
     }
-    print "\n"; # restore some semblance of formating
+  }
+  print "\n";			# restore some semblance of formating
 }
+close $old or die $!;
